70a9043a1038022e076fb9ead27c75e12222d2a2
saber <wenjie.0617@gmail.com>
2012-04-26 07:52:23 +0800

    add jna to invoke jni.

--- webstart/src/com/saber/jni/HelloJni.java
+++ webstart/src/com/saber/jni/HelloJni.java
@@ -1,7 +1,11 @@
 package com.saber.jni;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
 public class HelloJni {
-    public native void GetMacNo();
+/*    public native void GetMacNo();
 
     static {
         System.loadLibrary("COMM232");
@@ -10,5 +14,19 @@ public class HelloJni {
     public static void main(String[] args) {
         //System.out.println(System.getProperty("java.library.path"));
         new HelloJni().GetMacNo();
+    }*/
+	
+	// This is the standard, stable way of mapping, which supports extensive
+    // customization and mapping of Java to native types.
+
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.loadLibrary("COMM232", CLibrary.class);
+        void GetMacNo();
+    }
+
+    public static void main(String[] args) {
+    	System.out.println(System.getProperty("java.library.path"));
+    	int[] macNo = null;
+        CLibrary.INSTANCE.GetMacNo();
     }
 }
